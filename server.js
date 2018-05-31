8f4a85c5d857d449dd9e0dcdcc30b3f74dfad01f
nicktamburro <nicholastamburro@gmail.com>
2018-05-30 20:57:22 -0700

    added jsx doc, express server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 app.use(express.static('static'));
+//this 'static' parameter is referring to the directory names static, 
+//that's where it will find the index.html
 
 app.listen(3000, function() {
 	console.log("App started on port 3000");
