1f12867b67279654be99d6ded0edf8ec405baea5
nicktamburro <nicholastamburro@gmail.com>
2018-06-05 21:58:30 -0700

    converted some code to ES6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-var contentNode = document.getElementById('contents');
+const contentNode = document.getElementById('contents');
 
-var component = <h1>Hey dudes!</h1>;
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+const message = continents.map(c => `Hello ${c}!`).join(' ');
 
-
-
+const component = <p>{message}</p>;
 ReactDOM.render(component, contentNode);
