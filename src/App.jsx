adc866e51815a842a4942daac6a398e1a4f8298a
nicktamburro <nicholastamburro@gmail.com>
2018-09-10 22:13:18 -0700

    added ES^ template string

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ const contentNode = document.getElementById('contents');
 
 const continents = ['Africa', 'America', 'Kree Empire', 'Savage Land', 'Wakanda', 'Shaolin'];
 const message = continents.map(c => `Hello ${c}!`).join(' ');
+//here we've got a map. "c" is kind of like "i" in a for loop
+//we map each element from the continents array onto the message,
+//
+//when we use the `` backticks, we can use ${} to have a variable, and each value of "c"
+//gets inserted in there
 
 const component = <p>{message}</p>;
 ReactDOM.render(component, contentNode);
