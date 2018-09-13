a30aa0824722133a2937bfbc962efeac7f3a502a
nicktamburro <nicholastamburro@gmail.com>
2018-09-12 21:29:56 -0700

    deleted unused code

--- server.js
+++ server.js
@@ -1,13 +1,2 @@
-const express = require('express');
-//importing the npm express
 
-
-const app = express();
-app.use(express.static('static'));
-//this 'static' parameter is referring to the directory names static, 
-//that's where it will find the index.html
-
-app.listen(3000, function() {
-	console.log("App started on port 3000");
-})
 
