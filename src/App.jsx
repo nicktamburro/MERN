2126e6d0a94bf30d5e7430598d199df49dd7f50b
nicktamburro <nicholastamburro@gmail.com>
2018-09-14 13:48:34 -0700

    updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
 const contentNode = document.getElementById('contents');
 
+class IssueFilter extends React.Component{
+	render(){
+		return(
+			<div>IssueFilter will go here.</div>
+		)
+	}
+}
+
+class IssueTable extends React.Component{
+	render(){
+		return(
+			<div>Issue Table goes here</div>
+		)
+	}
+}
+
+class IssueAdd extends React.Component{
+	render(){
+		return(
+			<div>Issue Add goes here</div>
+		)
+	}
+}
+
 class IssueList extends React.Component{
 	render(){
 		return(
-			<div>things will go here</div>
+			<div>
+			<h1>Issue Tracker</h1>
+			<IssueFilter/>
+			<hr/>
+			<IssueTable/>
+			<hr/>
+			<IssueAdd />	
+			</div>
 		);
 	}
 }
+
+//these arguments say (<what thing?>, <where do we put it?>);
+ReactDOM.render(<IssueList />, contentNode);
 
 /*const continents = ['Savage Land', 'Wakanda', 'Latvertia', 'Danger Room'];
 const message = continents.map(c => `Hello ${c}!`).join(" ");
 */
 //const component = <p>{message}</p>;
+
+//these arguments say (<what thing?>, <where do we put it?>);
 ReactDOM.render(<IssueList />, contentNode);
